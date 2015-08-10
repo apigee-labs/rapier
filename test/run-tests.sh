6b06212d995fd8702807860563c947971a1bbc24
Martin Nally <mnally@apigee.com>
2015-08-10 13:52:45 -0700

    add bash script for running tests

---
+++ test/run-tests.sh
@@ -0,0 +1,4 @@
+python gen-swagger.py test/hello-message.yaml > test/swagger-hello-message.yaml
+python gen-swagger.py test/to-do-list.yaml > test/swagger-to-do-list.yaml
+python gen-swagger.py test/dog-tracker.yaml > test/swagger-dog-tracker.yaml
+python gen-swagger.py test/property-tracker.yaml > test/swagger-property-tracker.yaml
