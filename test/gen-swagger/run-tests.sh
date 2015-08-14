bf91621d10daf57e97c5f1a4993f4af29f4681ef
Martin Nally <mnally@apigee.com>
2015-08-14 11:01:02 -0700

    directory restructure

---
+++ test/gen-swagger/run-tests.sh
@@ -0,0 +1,4 @@
+python gen-swagger/gen-swagger.py test/gen-swagger/hello-message.yaml > test/gen-swagger/swagger-hello-message.yaml
+python gen-swagger/gen-swagger.py test/gen-swagger/to-do-list.yaml > test/gen-swagger/swagger-to-do-list.yaml
+python gen-swagger/gen-swagger.py test/gen-swagger/dog-tracker.yaml > test/gen-swagger/swagger-dog-tracker.yaml
+python gen-swagger/gen-swagger.py test/gen-swagger/property-tracker.yaml > test/gen-swagger/swagger-property-tracker.yaml
--- test/run-tests.sh
+++
@@ -1,4 +0,0 @@
-python gen-swagger/gen-swagger.py test/hello-message.yaml > test/swagger-hello-message.yaml
-python gen-swagger/gen-swagger.py test/to-do-list.yaml > test/swagger-to-do-list.yaml
-python gen-swagger/gen-swagger.py test/dog-tracker.yaml > test/swagger-dog-tracker.yaml
-python gen-swagger/gen-swagger.py test/property-tracker.yaml > test/swagger-property-tracker.yaml
