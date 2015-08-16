4ac8b9178c6243a6d3ee0a3f62621bd777243a13
Martin Nally <mnally@apigee.com>
2015-08-15 19:01:13 -0700

    directory rework

--- test/gen_swagger/run-tests.sh
+++ test/gen_swagger/run-tests.sh
@@ -1,4 +1,4 @@
-python gen_swagger/gen_swagger.py test/gen_swagger/hello-message.yaml > test/gen_swagger/swagger-hello-message.yaml
-python gen_swagger/gen_swagger.py test/gen_swagger/to-do-list.yaml > test/gen_swagger/swagger-to-do-list.yaml
-python gen_swagger/gen_swagger.py test/gen_swagger/dog-tracker.yaml > test/gen_swagger/swagger-dog-tracker.yaml
-python gen_swagger/gen_swagger.py test/gen_swagger/property-tracker.yaml > test/gen_swagger/swagger-property-tracker.yaml
+python gen_swagger/gen_swagger.py test/hello-message.yaml > test/gen_swagger/swagger-hello-message.yaml
+python gen_swagger/gen_swagger.py test/to-do-list.yaml > test/gen_swagger/swagger-to-do-list.yaml
+python gen_swagger/gen_swagger.py test/dog-tracker.yaml > test/gen_swagger/swagger-dog-tracker.yaml
+python gen_swagger/gen_swagger.py test/property-tracker.yaml > test/gen_swagger/swagger-property-tracker.yaml
