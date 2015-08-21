6cba8bab4d88db8b20eaf1d19986ebb4af2a5b3a
Martin Nally <mnally@apigee.com>
2015-08-20 18:55:11 -0700

    simplification

--- test/gen_python_sdk/hello_message/hello_message.py
+++ test/gen_python_sdk/hello_message/hello_message.py
@@ -2,10 +2,6 @@ from rapier.gen_python_sdk.base_entity import BaseEntity
 
 class HelloMessage(BaseEntity):
 
-    def __init__(self, json_representation = None, location = None, etag = None):
-        self.type = 'HelloMessage'
-        return super(Item, self).__init__(json_representation, location, etag)
-    
     def api(self):
         import hello_world_api
         return hello_world_api.api
