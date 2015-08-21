613398c7c33ffab71ae0f55134992c3003b0fae7
Martin Nally <mnally@apigee.com>
2015-08-20 20:06:15 -0700

    simplification

--- test/gen_python_sdk/todo_list/todo_list_api.py
+++ test/gen_python_sdk/todo_list/todo_list_api.py
@@ -2,7 +2,7 @@ from rapier.gen_python_sdk.base_api import BaseAPI
 from rapier.gen_python_sdk.base_entity import BaseEntity
 from rapier.gen_python_sdk.base_collection import BaseCollection
 
-class TodoListAPI(BaseAPI):
+class API(BaseAPI):
 
     def well_known_URLs(self):
         return ['/to-dos']
@@ -11,21 +11,21 @@ class TodoListAPI(BaseAPI):
         cls = globals().get(type_name)
         return cls if cls else BaseEntity
                     
-api = TodoListAPI()
+api = API()
 
-class TodoListAPIClass(object):
+class APIClass(object):
             
     def api(self):
         return api
         
-class TodoList(BaseEntity, TodoListAPIClass):
+class TodoList(BaseEntity, APIClass):
             
     pass
         
-class Item(BaseEntity, TodoListAPIClass):
+class Item(BaseEntity, APIClass):
     
     pass   
         
-class Collection(BaseCollection, TodoListAPIClass):
+class Collection(BaseCollection, APIClass):
     
     pass
