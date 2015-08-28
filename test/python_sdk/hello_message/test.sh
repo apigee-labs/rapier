c8c29793a71b4da25c0acfbbd5ca62528a083566
Martin Nally <mnally@apigee.com>
2015-08-28 11:22:34 -0700

    make test bash scripts independent of directory they are involed from

--- test/python_sdk/hello_message/test.sh
+++ test/python_sdk/hello_message/test.sh
@@ -1,3 +1,4 @@
-export PYTHONPATH=./:$PYTHONPATH
-#echo $PYTHONPATH
-python ~/rapier/test/python_sdk/hello_message/test.py
+DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd)
+ROOT_DIR=$( cd "$( dirname "$DIR/../../../../../" )" && pwd)
+export PYTHONPATH=$ROOT_DIR:$PYTHONPATH
+python "$DIR/test.py"
