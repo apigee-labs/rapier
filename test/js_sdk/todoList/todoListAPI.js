1c82a2d2ada828bda9fe0fbe6f21c423d5306196
Martin Nally <mnally@apigee.com>
2015-09-06 21:20:53 -0700

    tidying

--- test/js_sdk/todoList/todoListAPI.js
+++ test/js_sdk/todoList/todoListAPI.js
@@ -5,11 +5,9 @@ var todoListAPI = function() {
     function TodoListAPI() {}
     
     TodoListAPI.prototype = Object.create(baseAPI.BaseAPI.prototype);
-    
     TodoListAPI.prototype.well_known_URLs = function() {
         return ['/to-dos']
     }
-    
     TodoListAPI.prototype.resourceClass = function(type_name) {
         return  type_name in classToKindMap ? classToKindMap[type_name] : baseAPI.BaseEntity      
     }
@@ -23,29 +21,26 @@ var todoListAPI = function() {
     function TodoList(jso, url, etag) {
         baseAPI.BaseEntity.call(this, jso, url, etag)
     }
-    
     TodoList.prototype = Object.create(baseAPI.BaseEntity.prototype);
     TodoList.prototype.constructor = TodoList;
     TodoList.prototype._className = 'TodoList';
-    TodoList.prototype.api = api_function
+    TodoList.prototype.api = api_function;
     
     function Item(jso, url, etag) {
         baseAPI.BaseEntity.call(this, jso, url, etag)
     }
-    
     Item.prototype = Object.create(baseAPI.BaseEntity.prototype);
     Item.prototype.constructor = Item;
     Item.prototype._className = 'Item';
-    Item.prototype.api = api_function
+    Item.prototype.api = api_function;
         
     function Collection(jso, url, etag) {
         baseAPI.BaseCollection.call(this, jso, url, etag)
     }
-    
     Collection.prototype = Object.create(baseAPI.BaseCollection.prototype);
     Collection.prototype.constructor = Collection;
     Collection.prototype._className = 'Collection';
-    Collection.prototype.api = api_function
+    Collection.prototype.api = api_function;
             
     var classToKindMap = {
         TodoList: TodoList,
