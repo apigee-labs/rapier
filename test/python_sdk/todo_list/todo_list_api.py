6b0bc93de8876608aeb54b7a70e98227ab6f6909
Martin Nally <mnally@apigee.com>
2015-09-06 21:22:42 -0700

    tidying

--- test/python_sdk/todo_list/todo_list_api.py
+++ test/python_sdk/todo_list/todo_list_api.py
@@ -1,17 +1,14 @@
 from rapier.python_sdk.base_api import BaseAPI, BaseEntity, BaseCollection
 
 class API(BaseAPI):
-
     def well_known_URLs(self):
         return ['/to-dos']
-    
     def resource_class(self, type_name):
         return classToKindMap.get(type_name, BaseEntity)
                     
 api = API()
 
 class APIClass(object):
-            
     def api(self):
         return api
         
