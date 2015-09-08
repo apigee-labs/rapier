895148f991c700eef5fc3813b662fd3afb68575d
Martin Nally <mnally@apigee.com>
2015-09-07 19:42:44 -0700

    tidying

--- test/python_sdk/todo_list/test.py
+++ test/python_sdk/todo_list/test.py
@@ -4,16 +4,17 @@ def test_objects():
     todo_list = api.retrieve_well_known_resource('http://localhost:3001/to-dos')
     items = todo_list.retrieve('items')
     assert(isinstance(items, Collection))
-    new_item = Item({'description':'buy milk'})
+    new_item = Item()
+    new_item.description = 'buy milk'
     items.create(new_item)
     assert(hasattr(new_item, '_location') and new_item._location)                
     items.refresh()
     assert(new_item._location in items.items)
-    new_item.description = 'buy more milk'
+    new_item.description = 'buy gallon of milk'
     new_item.due = 'tonight'
     new_item.update()
     new_item.refresh()
-    assert(new_item.description == 'buy more milk')
+    assert(new_item.description == 'buy gallon of milk')
     new_item.delete()
     items.refresh()
     assert(new_item._location not in items.items)
@@ -23,20 +24,24 @@ def test_api():
     assert(hasattr(todo_list,'items'))
     items = api.retrieve('http://localhost:3001/to-dos/items')
     assert(isinstance(items, Collection))
-    new_item = Item({'description':'buy milk'})
-    items.create(new_item)
+    body = {
+        'kind': 'Item',
+        'description':'buy milk'
+        }
+    new_item = api.create('http://localhost:3001/to-dos/items', body)
     assert(hasattr(new_item, '_location') and new_item._location)                
-    items.refresh()
+    items = api.retrieve('http://localhost:3001/to-dos/items')
     assert(new_item._location in items.items)
-    new_item2 = items.items[new_item._location]
-    new_item2.description = 'buy more milk'
-    new_item2.due = 'tonight'
-    new_item2.update()
+    changes = {
+        'description': 'buy gallon of milk',
+        'due': 'tonight'
+        }
+    new_item2 = api.update(new_item._location, new_item._etag, changes)
     assert(new_item2._etag == str(int(new_item._etag) + 1))
-    assert(new_item2.description == 'buy more milk')
+    assert(new_item2.description == 'buy gallon of milk')
     new_item3 = api.delete(new_item._location)
     assert(new_item3._etag == new_item2._etag)
-    items.refresh()
+    items = api.retrieve('http://localhost:3001/to-dos/items')
     assert(new_item3._location not in items.items)
     
 def main():
