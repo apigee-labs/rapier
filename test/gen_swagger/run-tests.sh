a2e86d3777c27010a5612ab050a40001de8a965f
Martin Nally <mnally@apigee.com>
2015-09-08 17:02:37 -0700

    updated spec-hub swagger

--- test/gen_swagger/run-tests.sh
+++ test/gen_swagger/run-tests.sh
@@ -6,3 +6,4 @@ python rapier/gen_swagger/gen_swagger.py rapier/test/hello-message.yaml > rapier
 python rapier/gen_swagger/gen_swagger.py rapier/test/todo-list.yaml > rapier/test/gen_swagger/swagger-todo-list.yaml
 python rapier/gen_swagger/gen_swagger.py rapier/test/dog-tracker.yaml > rapier/test/gen_swagger/swagger-dog-tracker.yaml
 python rapier/gen_swagger/gen_swagger.py rapier/test/property-tracker.yaml > rapier/test/gen_swagger/swagger-property-tracker.yaml
+python rapier/gen_swagger/gen_swagger.py rapier/test/spec-hub.yaml > rapier/test/gen_swagger/swagger-spec-hub.yaml
