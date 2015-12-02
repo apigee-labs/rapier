9e9e861d455ed82b9d35500563a1b921e93bf80b
Martin Nally <mnally@apigee.com>
2015-12-02 15:31:19 -0800

    remove media-type property

--- util/gen_swagger.py
+++ util/gen_swagger.py
@@ -89,8 +89,6 @@ class SwaggerGenerator(object):
                             immutable_properties = {prop_name: prop for prop_name, prop in entity_spec['properties'].iteritems() if 'readOnly' in prop and prop['readOnly']}
                         if immutable_properties:
                             definition['properties'] = immutable_properties
-                    if 'media-type' in entity_spec:
-                        mutable_definition['x-media-type'] = entity_spec['media-type']
                     self.definitions['%sMutableProperties' % entity_name] = mutable_definition
                     if 'required' in entity_spec:
                         mutable_definition['required'] = entity_spec['required']
