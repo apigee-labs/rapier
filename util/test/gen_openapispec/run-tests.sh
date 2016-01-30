4cc5122f9f4195fade23e88c0dcdc1ae1e1cfc38
Martin Nally <mnally@apigee.com>
2016-01-29 16:13:26 -0800

    bash

--- util/test/gen_openapispec/run-tests.sh
+++ util/test/gen_openapispec/run-tests.sh
@@ -6,9 +6,9 @@ cd $ROOT_DIR
 ./rapier/util/gen_openapispec.py rapier/util/test/hello-message.yaml > rapier/util/test/gen_openapispec/openapispec-hello-message.yaml
 ./rapier/util/gen_openapispec.py rapier/util/test/todo-list-basic.yaml > rapier/util/test/gen_openapispec/openapispec-todo-list-basic.yaml
 ./rapier/util/gen_openapispec.py rapier/util/test/todo-list-with-id.yaml > rapier/util/test/gen_openapispec/openapispec-todo-list-with-id.yaml
-./rapier/util/gen_openapispec.py rapier/util/test/dog-tracker.yaml > rapier/util/test/gen_openapispec/openapispec-dog-tracker.yaml
 ./rapier/util/gen_openapispec.py rapier/util/test/todo-list-with-self.yaml > rapier/util/test/gen_openapispec/openapispec-todo-list-with-self.yaml
 ./rapier/util/gen_openapispec.py rapier/util/test/todo-list-with-links.yaml > rapier/util/test/gen_openapispec/openapispec-links-todo-list.yaml
+./rapier/util/gen_openapispec.py rapier/util/test/dog-tracker.yaml > rapier/util/test/gen_openapispec/openapispec-dog-tracker.yaml
 ./rapier/util/gen_openapispec.py rapier/util/test/property-tracker.yaml > rapier/util/test/gen_openapispec/openapispec-property-tracker.yaml
 ./rapier/util/gen_openapispec.py rapier/util/test/spec-hub.yaml > rapier/util/test/gen_openapispec/openapispec-spec-hub.yaml
 ./rapier/util/gen_openapispec.py -is rapier/util/test/spec-hub.yaml > rapier/util/test/gen_openapispec/openapispec-spec-hub-with-impl.yaml
