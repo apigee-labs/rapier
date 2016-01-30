b7759b605eaccfb7963390abc4ca83808ff219cc
Martin Nally <mnally@apigee.com>
2016-01-29 17:23:57 -0800

    remove non-resources

--- util/gen_openapispec.py
+++ util/gen_openapispec.py
@@ -85,19 +85,13 @@ class SwaggerGenerator(object):
             self.openapispec_uri_map = {'#/entities/%s' % name: '#/definitions/%s' % name for name in entities.iterkeys()}
             self.uri_map.update({'#/non_entity_resources/%s' % name: entity for name, entity in spec.get('non_entity_resources',{}).iteritems()})
             self.openapispec_uri_map.update({'#/non_entity_resources/%s' % name: '#/definitions/%s' % name for name in spec.get('non_entity_resources',{}).iterkeys()})
-            self.uri_map.update({'#/non_resources/%s' % name: entity for name, entity in spec.get('non_resources',{}).iteritems()})
-            self.openapispec_uri_map.update({'#/non_resources/%s' % name: '#/definitions/%s' % name for name in spec.get('non_resources',{}).iterkeys()})
             for entity in entities.itervalues():
                 if 'kind' not in entity:
                     entity['kind'] = 'Entity'
             for entity in spec.get('non_entity_resources',{}).itervalues():
                 if 'kind' not in entity:
                     entity['kind'] = 'NonEntityResource'                
-            for entity in spec.get('non_resources',{}).itervalues():
-                if 'kind' not in entity:
-                    entity['kind'] = 'NonResource'                
             entities.update(spec.get('non_entity_resources',{}))
-            entities.update(spec.get('non_resources',{}))
             if 'implementation_only' in spec:
                 for entity_name, entity in spec['implementation_only'].iteritems():
                     if 'properties' in entity:
