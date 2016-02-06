83527d88c0bf99aed51d0ad7ad7d94183833a215
Martin Nally <mnally@apigee.com>
2016-02-05 18:25:36 -0800

    begin implementation of a validator

---
+++ util/test/validate_rapier/run-validation-tests.sh
@@ -0,0 +1,17 @@
+DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd)
+# echo $DIR
+ROOT_DIR=$( cd "$( dirname "$DIR/../../../../../" )" && pwd)
+cd $ROOT_DIR
+# echo $ROOT_DIR
+./rapier/util/validate_openapispec.py rapier/util/test/validate_rapier/hello-message.yaml 
+#./rapier/util/validate_openapispec.py rapier/util/test/todo-list-basic.yaml
+#./rapier/util/validate_openapispec.py rapier/util/test/todo-list-with-id.yaml
+#./rapier/util/validate_openapispec.py rapier/util/test/todo-list-with-self.yaml
+#./rapier/util/validate_openapispec.py rapier/util/test/todo-list-with-links.yaml
+#./rapier/util/validate_openapispec.py rapier/util/test/dog-tracker.yaml
+#./rapier/util/validate_openapispec.py rapier/util/test/property-tracker.yaml
+#./rapier/util/validate_openapispec.py rapier/util/test/spec-hub.yaml
+#./rapier/util/validate_openapispec.py -is rapier/util/test/spec-hub.yaml
+#./rapier/util/validate_openapispec.py -s rapier/util/test/ssl.yaml
+#./rapier/util/validate_openapispec.py rapier/util/test/deployment.yaml
+#./rapier/util/validate_openapispec.py rapier/util/test/site-webmaster.yaml
