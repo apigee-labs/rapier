f521dcc62f7eb9a97ab77718f91ea0feecbc27b3
Martin Nally <mnally@apigee.com>
2016-03-03 12:21:46 -0700

    tweaking

--- util/gen_openapispec.py
+++ util/gen_openapispec.py
@@ -145,7 +145,7 @@ class OASGenerator(object):
                     interface = self.build_entity_interface(entity_url_spec)
                     interface['x-id'] = entity_url_spec.interface_id()
                     self.interfaces[entity_uri] = interface
-                    self.openapispec_interfaces[self.resolve_entity_name(entity_uri)] = interface
+                    self.openapispec_interfaces[entity_url_spec.interface_id()] = interface
                     self.openapispec_templates[entity_url_spec.template_id()] = self.build_interface_reference(entity_url_spec)
                     rel_property_specs = self.get_entity_relationship_property_specs(entity_uri, entity_spec)
                     for rel_property_spec in rel_property_specs:
