2418f918f749e25e36ab76a6425abcc0144b0d1e
Martin Nally <mnally@apigee.com>
2016-09-14 09:51:44 -0700

    add setup.cfg

--- setup.py
+++ setup.py
@@ -1,16 +1,16 @@
 from setuptools import setup
  
 setup(
-    name = "rapier",
-    packages = ["util"],
+    name = 'rapier',
+    packages = ['util'],
     entry_points = {
-        "console_scripts": ['rapier = util.rapier:main']
+        'console_scripts': ['rapier = util.rapier:main']
         },
-    version = "0.0.1",
-    description = "Generate OpenAPI specification from Rapier specification",
-    long_description = "Generate OpenAPI specification from Rapier specification",
-    author = "Martin Nally",
-    author_email = "mnally@apigee.com",
-    url = "https://github.com/apigee-labs/rapier",
+    version = '0.0.1',
+    description = 'Generate OpenAPI specification from Rapier specification',
+    long_description = 'Generate OpenAPI specification from Rapier specification',
+    author = 'Martin Nally',
+    author_email = 'mnally@apigee.com',
+    url = 'https://github.com/apigee-labs/rapier',
     install_requires = ['PyYAML==3.11']
     )
